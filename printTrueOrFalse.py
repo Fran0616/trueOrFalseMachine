57a96609950863d4c4d254c28778fbf705c329f1
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-19 19:35:32 -0400

    Add files via upload

---
+++ printTrueOrFalse.py
@@ -0,0 +1,10 @@
+#this code will take the parameter n as an input, and print false if n is less than 100, and true if n is greater than or eaqual to 100 
+
+n = float(input("Enter a number: "))
+print(n>=100)
+
+#sample input: 8
+#expected output: False
+
+#sample input: 100
+#expected output: True
